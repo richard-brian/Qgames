9542d5934e2e8bb1b8afa59b351b8c578907bc46
Arthur <arthurantoniobraga@gmail.com>
2026-06-16 16:31:27 -0300

    Update da tela de cadastro

---
+++ QGames/Js/cadastro-login.js
@@ -0,0 +1,15 @@
+
+        document.getElementById('form-cadastro').addEventListener('submit', function(event) {
+            // Evita que a página recarregue ao apertar Enter
+            event.preventDefault();
+
+            // Pegando os valores preenchidos (opcional, para validação)
+            const email = document.getElementById('email').value;
+            const user = document.getElementById('username').value;
+            
+            console.log('Enter pressionado! Formulário validado para:', email);
+
+            // AQUI VOCÊ COLOCA O CÓDIGO DA PRÓXIMA ETAPA
+            // Exemplo: window.location.href = "proxima-etapa.html";
+            // Ou chamar uma função que esconde este card e mostra outro.
+        });
