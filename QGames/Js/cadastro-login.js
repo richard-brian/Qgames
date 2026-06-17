8daedb7949133b22f34cebd9b642faa46284cb6e
Arthur <arthurantoniobraga@gmail.com>
2026-06-17 09:05:35 -0300

    Update no perfil 1/2

--- QGames/Js/cadastro-login.js
+++ QGames/Js/cadastro-login.js
@@ -1,32 +1,49 @@
 
 const formCadastro = document.getElementById('form-cadastro');
-
-
 if (formCadastro) {
-    formCadastro.addEventListener('submit', function(event) {
-        event.preventDefault(); 
-        
-        console.log('Enter pressionado no Cadastro!');
+    formCadastro.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const emailCadastro = document.getElementById('email').value;
+        const senhaCadastro = document.getElementById('floatingPassword').value;
+        const usuario = document.getElementById('username').value;
+        localStorage.setItem('nomeUsuario', usuario);
+
+        console.log('`Pressione enter para prosseguir`!');
         window.location.href = "paginaQuiz.html"
+
+    });
+}
+
+const formLogin = document.getElementById('form-login');
+if (formLogin) {
+    formLogin.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+
+        const emailLogin = document.getElementById('email').value;
+        const senhaLogin = document.getElementById('floatingPassword').value;
         
+        console.log('Pressione enter para continuar', emailLogin);
+
+        window.location.href = "paginaQuiz.html"
     });
 }
 
 
 
-const formLogin = document.getElementById('form-login');
 
+// Recupera o valor salvo no localStorage
+const usuario = localStorage.getItem('nomeUsuario');
 
-if (formLogin) {
-    formLogin.addEventListener('submit', function(event) {
-        event.preventDefault(); 
-        
-        
-        const emailLogin = document.getElementById('email').value;
-        const senhaLogin = document.getElementById('floatingPassword').value;
-        
-        console.log('Enter pressionado no Login! Tentando conectar com:', emailLogin);
-        
-        window.location.href = "paginaQuiz.html"
-    });
+// Seleciona a div
+const perfilusername = document.getElementById('perfil-username');
+
+// Se o usuário existir, insere no HTML
+if (usuario) {
+    perfilusername.innerHTML += `<h1>${usuario}</h1>`;
+    
+} 
+else {
+    perfilusername.innerHTML += `<h1>Visitante</h1>`; // Caso não encontre o cadastro
 }
